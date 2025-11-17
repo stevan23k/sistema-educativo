693456f53a025bb47f4ed4c585ce25d81c0eac4e
stevan23k <esteproo@gmail.com>
2025-11-16 22:43:30 -0500

    autenticacion
    inplementacion de encriptacion de contraseñas y generacion de token jwt.

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports: [
+    JwtModule.register({
+      global: true,
+      secret: 'llave secreta',
+      signOptions: { expiresIn: '7d' },
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,43 +2,57 @@ import { Injectable } from '@nestjs/common';
 import { Usuarios, Roles } from './auth.model';
 import { LoginDTO } from './DTO/LoginDTO';
 import { RegistroDTO } from './DTO/RegistroDTO';
+import { hashSync, compareSync } from 'bcrypt';
+import { HttpException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
+  constructor(private jwt: JwtService) {}
   private db: Usuarios[] = [
     {
       id: 1,
       nombres: 'juan camilo',
       apellidos: 'cardona murillos',
       email: 'jcardona@mail.com',
-      password: 'ejemplo',
+      password: '$2b$10$vkeENHDS/aU0q7239YffL.Oq6OTmiuHeWaQQrM24jJxgGNHE9HMce',
       rol: Roles.estudiante,
     },
   ];
 
-  login(user: LoginDTO) {
+  async login(user: LoginDTO) {
     const usuario = this.db.find((usuario) => usuario.email === user.email);
 
     if (!usuario) {
-      return { error: 'usuario no encontrado' };
+      throw new HttpException('Usuario no encontrado', 404);
     }
 
-    if (user.email === usuario.email && user.password === usuario.password) {
-      return { ok: 'login exitoso' };
-    } else {
-      return { error: 'error al iniciar sesion' };
+    const password = compareSync(user.password, usuario.password);
+    if (!password) {
+      throw new HttpException('Contraseña incorrecta', 401);
     }
+
+    const payload = {
+      id: usuario.id,
+      nombres: usuario.nombres,
+      email: usuario.email,
+      rol: usuario.rol,
+    };
+
+    return { token: await this.jwt.signAsync(payload) };
   }
 
   registro(user: RegistroDTO) {
+    const passwordEncrypted = hashSync(user.password, 10);
     const newUser = {
       id: this.db.length + 1,
       ...user,
+      password: passwordEncrypted,
     };
 
     this.db.push(newUser);
 
-    return { ok: 'usuario registrado exitosamente' };
+    return { ok: 'usuario registrado exitosamente', newUser };
   }
 
   getUsers() {
